3e19f6a5a9cadbd4646e2ee76661f19fa962970a
sakhalin <73.vorobyev@gmail.com>
2019-07-07 14:28:44 +0700

    Update police view

--- src/pages/police/route-sheet/View.jsx
+++ src/pages/police/route-sheet/View.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
-import {Button} from '@material-ui/core';
 import {formatDate, formatTime} from '../../../utils/format';
 import route_sheets from '../../../data/router-sheets';
 
